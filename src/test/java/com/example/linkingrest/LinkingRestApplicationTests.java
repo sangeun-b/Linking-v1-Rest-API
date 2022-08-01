b530e18cbafede53dc7e5969d8eb93d5e2863739
sangeun-b <baek0007@algonquinlive.com>
2022-08-02 04:23:10 +0900

    fixed setting

--- src/test/java/com/example/linkingrest/LinkingRestApplicationTests.java
+++ src/test/java/com/example/linkingrest/LinkingRestApplicationTests.java
@@ -4,8 +4,11 @@ import com.example.linkingrest.config.security.JwtProvider;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test-jwt")
 class LinkingRestApplicationTests {
 
     @Test
